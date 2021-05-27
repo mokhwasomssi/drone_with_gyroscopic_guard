3f62a065e4a27aa6a97fdfa7e4e7050c178a5a09
석은혜 <mokhwasomssi@naver.com>
2021-05-28 00:36:16 +0900

    Update dshot.h/.c
    - refatoring
    - problem : each signal is out of sync

--- firmware/Lib/Inc/dshot.h
+++ firmware/Lib/Inc/dshot.h
@@ -7,60 +7,100 @@
  * 
  */
 
+
 #ifndef _DSHOT_H_
 #define _DSHOT_H_
 
-#include "stm32f4xx_hal.h"
-#include "tim.h"
-#include <stdbool.h>
+
+#include "tim.h"    // header from stm32cubemx code generate
 
 
-/* User Configuration */
-// MOTOR1 : PA2
-#define MOTOR_1_TIM             (&htim2)
-#define MOTOR_1_TIM_CHANNEL     TIM_CHANNEL_3
+/* user pin setting */
+// MOTOR1 : PA3
+#define MOTOR_1_TIM             (&htim5)
+#define MOTOR_1_TIM_CHANNEL     TIM_CHANNEL_4
 
-// MOTOR2 : PA3
-#define MOTOR_2_TIM             (&htim5)
-#define MOTOR_2_TIM_CHANNEL     TIM_CHANNEL_4
+// MOTOR2 : PA2
+#define MOTOR_2_TIM             (&htim2)
+#define MOTOR_2_TIM_CHANNEL     TIM_CHANNEL_3
 
-// MOTOR3 : PA1
-#define MOTOR_3_TIM             (&htim5)
-#define MOTOR_3_TIM_CHANNEL     TIM_CHANNEL_2
+// MOTOR3 : PA0
+#define MOTOR_3_TIM             (&htim2)
+#define MOTOR_3_TIM_CHANNEL     TIM_CHANNEL_1
 
-// MOTOR4 : PA0
-#define MOTOR_4_TIM             (&htim2)
-#define MOTOR_4_TIM_CHANNEL     TIM_CHANNEL_1
-/* User Configuration */
+// MOTOR4 : PA1
+#define MOTOR_4_TIM             (&htim5)
+#define MOTOR_4_TIM_CHANNEL     TIM_CHANNEL_2
 
+
+/* definition */
+#define NUM_OF_MOTORS          4    // quadcopter
+
+#define MHZ_TO_HZ(x) ((x) * 1000000)
+
+#define DSHOT600_HZ     MHZ_TO_HZ(12)
+#define DSHOT300_HZ     MHZ_TO_HZ(6)
+#define DSHOT150_HZ     MHZ_TO_HZ(3)
 
 #define MOTOR_BIT_0            7
 #define MOTOR_BIT_1            14
 #define MOTOR_BITLENGTH        20
 
 #define DSHOT_FRAME_SIZE       18
+#define DSHOT_DMA_BUFFER_SIZE   18 /* resolution + frame reset (2us) */
 
-#define NUM_OF_MOTORS          4    // quadcopter
+#define DSHOT_MIN_THROTTLE       48
+#define DSHOT_MAX_THROTTLE     2047
+#define DSHOT_RANGE (DSHOT_MAX_THROTTLE - DSHOT_MIN_THROTTLE)
 
-#define DSHOT_THROTTLE_STOP    0
-#define DSHOT_THROTTLE_MIN     48
-#define DSHOT_THROTTLE_MAX     2047
 
-typedef uint32_t dshot_frame;
-typedef uint16_t throttle_a;
-
-typedef struct _motors
+/* enumeration */
+typedef enum
 {
-    dshot_frame motor_1[DSHOT_FRAME_SIZE];
-    dshot_frame motor_2[DSHOT_FRAME_SIZE];
-    dshot_frame motor_3[DSHOT_FRAME_SIZE];
-    dshot_frame motor_4[DSHOT_FRAME_SIZE];
+    DSHOT150,
+    DSHOT300,
+    DSHOT600
 
-} motors_s;
+} dshot_type_e;
 
-void init_dshot();
-void make_dshot600_frame(dshot_frame *motor_x, throttle_a value);
-void run_dshot600(motors_s *motors, throttle_a value[]);
+
+/* structure */
+typedef struct dshot_handle_s
+{
+	TIM_HandleTypeDef* 	dshot_timer;
+	uint32_t 			channel;			
+	
+	uint16_t 			value;
+	bool 				request_telemetry;
+
+	uint16_t 			packet;
+	uint32_t 			dshot_dmabuffer[DSHOT_DMA_BUFFER_SIZE];
+
+} dshot_handle_t;
+
+
+/* variable */
+dshot_handle_t dshot_handle[4];
+
+
+/* functions */
+uint32_t dshot_choose_type(dshot_type_e dshot_type);
+
+void dshot_set_timer(dshot_handle_t *dshot_handle_array, dshot_type_e dshot_type);
+
+void dshot_init(dshot_handle_t *dshot_handle_array, dshot_type_e dshot_type);
+
+void dshot_prepare_packet(dshot_handle_t *dshot_handle, uint16_t value);
+
+void dshot_packet_to_pwm(dshot_handle_t *dshot_handle);
+
+void dshot_dma_start_hal(dshot_handle_t *dshot_handle);
+
+void dshot_dma_start(TIM_HandleTypeDef* dshot_timer, uint32_t channel, uint32_t *pData, uint16_t length);
+
+void dshot_write(dshot_handle_t *dshot_handle_array, uint16_t value, uint8_t index);
+
+void dshot_write_all(dshot_handle_t *dshot_handle_array, uint16_t *value);
 
 
 #endif /* _DSHOT_H_ */
--- firmware/Lib/Src/dshot.c
+++ firmware/Lib/Src/dshot.c
@@ -7,62 +7,161 @@
  *
  */
 
+
 #include "dshot.h"
 
-void init_dshot(motors_s *motors)
+
+uint32_t dshot_choose_type(dshot_type_e dshot_type)
 {
-  // it have to send 0 value of dshot frame for a while to reset
-  // regardless of RC receiver value
+	switch (dshot_type)
+	{
+	case(DSHOT600):
+			printf("dshot type : DSHOT600\n");
+			return DSHOT600_HZ;
+	case(DSHOT300):
+		printf("dshot type : DSHOT300\n");
+			return DSHOT300_HZ;
+
+	default:
+	case(DSHOT150):
+			printf("dshot type : DSHOT150\n");
+			return DSHOT150_HZ;
+	}
 }
 
-void make_dshot600_frame(dshot_frame *motor_x, throttle_a value)
+void dshot_set_timer(dshot_handle_t *dshot_handle_array, dshot_type_e dshot_type)
 {
-  uint16_t packet = value << 1;
+  uint16_t dshot_prescaler;
+  uint32_t timer_clock = SystemCoreClock;
 
-  // compute checksum
-  int csum = 0;
-  int csum_data = packet;
-  for (int i = 0; i < 3; i++) {
-    csum ^=  csum_data;   // xor data by nibbles
-    csum_data >>= 4
-    
-    
-    ;
-  }
-  csum &= 0xf;
+  dshot_prescaler = lrintf((float) timer_clock / dshot_choose_type(dshot_type) + 0.01f) - 1;
 
-  // append checksum
-  packet = (packet << 4) | csum;
-
-  // encoding
-  int i;
-  for (i = 0; i < 16; i++)
+  for(int i = 0; i < 4; i++)
   {
-      motor_x[i] = (packet & 0x8000) ? MOTOR_BIT_1 : MOTOR_BIT_0;  // MSB first
-      packet <<= 1;
+	  __HAL_TIM_SET_PRESCALER(dshot_handle[i].dshot_timer, dshot_prescaler);
+	  __HAL_TIM_SET_AUTORELOAD(dshot_handle[i].dshot_timer, MOTOR_BITLENGTH);
   }
-
-  motor_x[i++] = 0;
-  motor_x[i++] = 0;
 }
 
-void run_dshot600(motors_s *motors, throttle_a value[])
+void dshot_init(dshot_handle_t *dshot_handle_array, dshot_type_e dshot_type)
 {
-  // prepare dshot frame
-  make_dshot600_frame(motors->motor_1, value[0]);
-  make_dshot600_frame(motors->motor_2, value[1]);
-  make_dshot600_frame(motors->motor_3, value[2]);
-  make_dshot600_frame(motors->motor_4, value[3]);
+	dshot_handle[0].dshot_timer = MOTOR_1_TIM;
+	dshot_handle[1].dshot_timer = MOTOR_2_TIM;
+	dshot_handle[2].dshot_timer = MOTOR_3_TIM;
+	dshot_handle[3].dshot_timer = MOTOR_4_TIM;
 
-  // and send
-  HAL_TIM_PWM_Start_DMA(MOTOR_1_TIM, MOTOR_1_TIM_CHANNEL, motors->motor_1, DSHOT_FRAME_SIZE);
-  HAL_TIM_PWM_Start_DMA(MOTOR_2_TIM, MOTOR_2_TIM_CHANNEL, motors->motor_2, DSHOT_FRAME_SIZE);
+	dshot_handle[0].channel = MOTOR_1_TIM_CHANNEL;
+	dshot_handle[1].channel = MOTOR_2_TIM_CHANNEL;
+	dshot_handle[2].channel = MOTOR_3_TIM_CHANNEL;
+	dshot_handle[3].channel = MOTOR_4_TIM_CHANNEL;
 
-  // commit : a0fc3be487dbd174be31abf8ce6e4e3b70c7a07b
-  __HAL_TIM_RESET_HANDLE_STATE(&htim5);
-  __HAL_TIM_RESET_HANDLE_STATE(&htim2);
+	dshot_set_timer(dshot_handle_array, dshot_type);
 
-  HAL_TIM_PWM_Start_DMA(MOTOR_3_TIM, MOTOR_3_TIM_CHANNEL, motors->motor_3, DSHOT_FRAME_SIZE);
-  HAL_TIM_PWM_Start_DMA(MOTOR_4_TIM, MOTOR_4_TIM_CHANNEL, motors->motor_4, DSHOT_FRAME_SIZE);
+	printf("dshot initialized\n");
 }
+
+void dshot_prepare_packet(dshot_handle_t *dshot_handle, uint16_t value)
+{
+	uint16_t packet;
+	bool dshot_telemetry = false;
+
+	packet = (value << 1) | (dshot_telemetry ? 1 : 0);
+
+	// compute checksum
+	unsigned csum = 0;
+	unsigned csum_data = packet;
+
+	for(int i = 0; i < 3; i++)
+	{
+        csum ^=  csum_data;   // xor data by nibbles
+        csum_data >>= 4;
+	}
+
+	csum &= 0xf;
+	dshot_handle->packet = (packet << 4) | csum;
+}
+
+void dshot_packet_to_pwm(dshot_handle_t *dshot_handle)
+{
+	int i;
+	uint16_t packet = dshot_handle->packet;
+
+	for(i = 0; i < 16; i++)
+	{
+		dshot_handle->dshot_dmabuffer[i] = (packet & 0x8000) ? MOTOR_BIT_1 : MOTOR_BIT_0;
+		packet <<= 1;
+	}
+
+	dshot_handle->dshot_dmabuffer[i++] = 0;
+	dshot_handle->dshot_dmabuffer[i++] = 0;
+
+}
+
+void dshot_dma_start_hal(dshot_handle_t *dshot_handle)
+{
+	HAL_TIM_PWM_Start_DMA(dshot_handle->dshot_timer, dshot_handle->channel, dshot_handle->dshot_dmabuffer, 18);
+
+}
+
+
+void dshot_dma_start(TIM_HandleTypeDef *htim, uint32_t channel, uint32_t *pData, uint16_t Length)
+{
+
+
+    switch (channel) {
+    case TIM_CHANNEL_1:
+        HAL_DMA_Start(htim->hdma[TIM_DMA_ID_CC1], (uint32_t)pData, (uint32_t)&htim->Instance->CCR1, Length);
+        __HAL_TIM_ENABLE_DMA(htim, TIM_DMA_CC1);
+    break;
+
+    case TIM_CHANNEL_2:
+        HAL_DMA_Start(htim->hdma[TIM_DMA_ID_CC2], (uint32_t)pData, (uint32_t)&htim->Instance->CCR2, Length);
+        __HAL_TIM_ENABLE_DMA(htim, TIM_DMA_CC2);
+        break;
+
+    case TIM_CHANNEL_3:
+        HAL_DMA_Start(htim->hdma[TIM_DMA_ID_CC3], (uint32_t)pData, (uint32_t)&htim->Instance->CCR3,Length);
+        __HAL_TIM_ENABLE_DMA(htim, TIM_DMA_CC3);
+        break;
+
+    case TIM_CHANNEL_4:
+        HAL_DMA_Start(htim->hdma[TIM_DMA_ID_CC4], (uint32_t)pData, (uint32_t)&htim->Instance->CCR4, Length);
+        __HAL_TIM_ENABLE_DMA(htim, TIM_DMA_CC4);
+        break;
+    }
+	
+
+}
+
+void dshot_write(dshot_handle_t *dshot_handle_array, uint16_t value, uint8_t index)
+{
+	dshot_prepare_packet(&dshot_handle[index], value);
+
+	dshot_packet_to_pwm(&dshot_handle[index]);
+
+	//dshot_dma_start_hal(&dshot_handle[index]);
+
+	//dshot_dma_start(dshot_handle[index].dshot_timer, dshot_handle[index].channel, dshot_handle[index].dshot_dmabuffer, 18);
+}
+
+void dshot_write_all(dshot_handle_t *dshot_handle_array, uint16_t *value)
+{
+	for(int i = 0; i < 4; i++)
+	{
+		dshot_write(&dshot_handle[i], i, value[i]);
+	}
+
+
+	for(int i = 0; i < 4; i++)
+	{
+		dshot_dma_start_hal(&dshot_handle[i]);
+	}
+
+}
+
+
+
+
+
+
 
